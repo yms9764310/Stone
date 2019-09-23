9fdcc9c2c70c29433802f6b0c93b93aefd47c11a
yzj <1370173383@qq.com>
2019-09-23 16:10:37 +0800

    第三此 2019.9.14

--- src/main/java/com/jc/model/TaccountHandleBill.java
+++ src/main/java/com/jc/model/TaccountHandleBill.java
@@ -6,9 +6,8 @@ import lombok.Data;
 import java.util.Date;
 
 @Data
-public class TaccountPayBill {
-    private Integer id;//部门主键
-    private Integer did;
+public class TaccountHandleBill {
+    private Integer id;//主键
     private Integer creator;//创建人
     private  String creator_name;
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
@@ -18,5 +17,11 @@ public class TaccountPayBill {
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     private Date modify_date;//修改时间
     private String state;//状态
-
+    private Integer commit_user_id;
+    private Integer effect_user_id;
+    private double sum_money;//金额
+    private Data pay_date;//应付日期
+    private String account_no;//账号
+    private String source_type;//来源但类型
+    private Integer source_id;//来源单
 }
